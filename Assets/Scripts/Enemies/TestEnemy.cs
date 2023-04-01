b9777a21af5bdf1ed2af0866e30c2ff98150f4c0
Pkepowicz <paw.kepowicz@gmail.com>
2023-04-01 18:48:32 +0200

    working mobs

--- Assets/Scripts/Enemies/TestEnemy.cs
+++ Assets/Scripts/Enemies/TestEnemy.cs
@@ -4,30 +4,52 @@ using UnityEngine;
 
 public class TestEnemy : Fighter
 {
-    public float moveSpeed = 2.0f;
-    private Vector2 waypoint;
-    public float speed = 10f;
-    public float maxMove = 30f;
+    public float moveSpeedX = 2.0f;
+    public float moveSpeedY = 2.0f;
+    public Vector3 stationaryPosition;
+    public float horizontalSpeed = 10f;
+    public float verticalSpeed = 10f;
+    public float maxHorizontalMove = 30f;
+    public float maxVerticalMove = 2.0f;
+    private bool isOnPosition;
+    public float tolerance = 0.1f;
 
     protected override void Start()
     {
         base.Start();
-        waypoint = transform.position;
     }
     
     protected override void Update()
     {
         base.Update();
         // code that moves object in x axis and changes direction when moves more than maxMove from waypoint location
-        if (transform.position.x < waypoint.x - maxMove)
+        if(!isOnPosition)
         {
-            moveSpeed = speed;
+            Vector3 direction = stationaryPosition - transform.position;
+            transform.position += direction.normalized * horizontalSpeed * Time.deltaTime;
+            if(direction.magnitude <= tolerance)
+            {
+                isOnPosition = true;
+            }
+            return;
         }
-        if (transform.position.x > waypoint.x + maxMove)
+        if (transform.position.x < stationaryPosition.x - maxHorizontalMove)
         {
-            moveSpeed = -speed;
+            moveSpeedX = horizontalSpeed;
         }
-        transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+        if (transform.position.x > stationaryPosition.x + maxHorizontalMove)
+        {
+            moveSpeedX = -horizontalSpeed;
+        }
+        if (transform.position.y < stationaryPosition.y - maxVerticalMove)
+        {
+            moveSpeedY = verticalSpeed;
+        }
+        if (transform.position.y > stationaryPosition.y + maxVerticalMove)
+        {
+            moveSpeedY = -verticalSpeed;
+        }
+        transform.Translate(moveSpeedX * Time.deltaTime, moveSpeedY * Time.deltaTime, 0);
     }
 }
         
