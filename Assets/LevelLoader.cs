58c26ff9bcbbec4e6c7c1d51d59661e6a143b77a
Pkepowicz <paw.kepowicz@gmail.com>
2023-04-02 07:31:27 +0200

    Scene animations

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public static LevelLoader Instance;
+    public float transitionTime = 0.75f;
+
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void LoadNextLevel(string scene)
+    {
+        StartCoroutine(LoadLevel(scene));
+    }
+
+    private IEnumerator LoadLevel(string scene)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+    }
+}
